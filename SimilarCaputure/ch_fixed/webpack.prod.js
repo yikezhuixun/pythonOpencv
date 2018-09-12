3b01c25398878756f9d151333e936efd8ca5647a
yikezhuixun <527027630@qq.com>
2018-09-12 08:22:42 +0800

    remove others
    remove others

--- SimilarCaputure/ch_fixed/webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const webpack = require("webpack");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const baseConfig = require("./webpack.base.js");
-module.exports = function (env, args) {
-    const prodConfig = baseConfig(env, args);
-    prodConfig.plugins.splice(0, 0, new CleanWebpackPlugin(['dist']));
-    prodConfig.mode = "production";
-    return prodConfig;
-}
